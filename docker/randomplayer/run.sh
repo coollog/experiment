6479a0cb73d1f16c6b41b4171c2bbfbdb7215817
Qingyang Chen <qingyangc@google.com>
2019-01-18 15:01:33 -0500

    cannot call service server

--- docker/randomplayer/run.sh
+++ docker/randomplayer/run.sh
@@ -6,16 +6,16 @@ while true; do
     CHOICE=$((1 + RANDOM % 4))
 
     if [ "$CHOICE" -eq "1" ]; then
-       curl http://server/move/me/up
+       curl http://game-server/move/me/up
     fi
     if [ "$CHOICE" -eq "2" ]; then
-       curl http://server/move/me/down
+       curl http://game-server/move/me/down
     fi
     if [ "$CHOICE" -eq "3" ]; then
-       curl http://server/move/me/left
+       curl http://game-server/move/me/left
     fi
     if [ "$CHOICE" -eq "4" ]; then
-       curl http://server/move/me/right
+       curl http://game-server/move/me/right
     fi
 
     sleep 0.3
